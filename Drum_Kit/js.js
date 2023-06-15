be2d5900b1e575e3d0de553d7a267b4901123696
Konrad <ko.kostrzebski@gmail.com>
2023-06-15 10:16:08 +0200

    end

--- Drum_Kit/js.js
+++ Drum_Kit/js.js
@@ -1,3 +1,20 @@
-window.addEventListener('keydown', function(e){
-    console.log(e);
-})
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return // skip it if it's not a transform
+    e.target.classList.remove('playing')
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
+
